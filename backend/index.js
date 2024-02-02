7a3e75d8c936e37983086edd0c68eaeac7d0b7dd
amit singh <singhamit2761@gmail.com>
2024-02-02 22:23:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 const express = require('express')
 require("dotenv").config()
 
-const startMongoDB=require('./db');
+const startMongoDB = require('./db');
 startMongoDB();
 const app = express()
 const port = 5000;
 
 
-app.use('/',(req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin",process.env.ONLINE_URL);
-  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+app.use('/', (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://swigato-backend-xe1m.onrender.com");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
   next();
 });
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', process.env.ONLINE_URL);
+  res.header('Access-Control-Allow-Origin', "https://swigato-backend-xe1m.onrender.com");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.send();
@@ -26,10 +26,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use('/api',require("./routes/CreateUser"));
-app.use('/api',require("./routes/DisplayData"));
-app.use('/api',require("./routes/OrderData"));
-app.use('/api',require("./routes/FindUser"));
+app.use('/api', require("./routes/CreateUser"));
+app.use('/api', require("./routes/DisplayData"));
+app.use('/api', require("./routes/OrderData"));
+app.use('/api', require("./routes/FindUser"));
 app.listen(port, () => {
   console.log(` app listening on port ${port}`)
 })
